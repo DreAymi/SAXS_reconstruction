488b3617cf7bb1363381513501c266f9f44fa879
haohe <810975814@qq.com>
2019-08-06 23:44:15 +0800

    add sample

--- G_run_single.sh
+++ G_run_single.sh
@@ -1,5 +1,14 @@
-iqpath=SASDAH6.out
-pdbpath=SASDAH6_fit1_model1.pdb
+iqpath=SASDA25.out
+
 outpath=result
-sastbx.python ../main.py > $outpath/logprint.txt --iq_path $iqpath --output_folder $outpath --rmax 55.5 --target_pdb $pdbpath
-#CUDA_VISIBLE_DEVICES=1 sastbx.python ../main.py > $outpath/logprint.txt --iq_path $iqpath --output_folder $outpath --rmax 55.5 --target_pdb $pdbpath
+sastbx.python main.py > $outpath/logprint.txt --iq_path $iqpath --output_folder $outpath
+
+#give rmax
+#rmax=35
+#sastbx.python main.py > $outpath/logprint.txt --iq_path $iqpath --output_folder $outpath --rmax $rmax
+
+#if you want to compare with exist pdb structure
+#pdbpath=SASDA25_fit1_model1.pdb
+#sastbx.python main.py > $outpath/logprint.txt --iq_path $iqpath --output_folder $outpath --rmax 35 --target_pdb $pdbpath
+
+#CUDA_VISIBLE_DEVICES=1 sastbx.python main.py > $outpath/logprint.txt --iq_path $iqpath --output_folder $outpath --rmax 55.5 --target_pdb $pdbpath
